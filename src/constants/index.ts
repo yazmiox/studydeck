30b22abc392c270c5b18b041906f95d6ed20b33e
Yasin Ahmed <myselfyasinahmed@gmail.com>
2026-04-29 23:24:46 +0600

    chore: updated app version

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -14,7 +14,7 @@ export const STATUS_LABELS = {
 
 export const APP_CONFIG = {
     name: "StudyDeck",
-    version: "2.0.4",
+    version: "2.0.5",
     author: "Yasin Ahmed",
     sourceCodeUrl: "https://github.com/yazmiox/studydeck",
     repoUrl: "https://github.com/yazmiox/studydeck",
